7027a286f360e78a0c92d946515375b96db4dee5
AKASH KUMAR <akash.kr@outlook.com>
2024-10-09 03:51:06 +0530

    Added new feature class

---
+++ src/Features.java
@@ -0,0 +1,15 @@
+class Features {
+    public void m1(){
+        System.out.println("this is new feature");
+
+    }
+    public void m2(){
+        System.out.println("this is new feature");
+
+    }
+    public void m1(int x){
+        System.out.println("this is new feature");
+
+
+    }
+}
